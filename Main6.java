94ee9352dbe1d9d27b87d3a5d38c3f4a86c15921
viljamivuorre <122791946+viljamivuorre@users.noreply.github.com>
2023-01-17 15:11:52 +0200

    Add files via upload

---
+++ Main6.java
@@ -0,0 +1,45 @@
+package T6;
+
+import java.util.Scanner;
+public class Main6 {
+	
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Sanalista sanalista = new Sanalista("sanoja.txt");
+		String input;
+		int  x = 0;
+		do {
+			System.out.println("syötä arvauskertojen määrä: ");
+			input = sc.nextLine();
+			try 
+			{ 
+				x = Integer.parseInt(input); 
+			}  
+			catch (NumberFormatException e)  
+			{ 
+				x = 0;
+			} 
+		} while (x <= 0);
+		Hirsipuu hirsipuu = new Hirsipuu(sanalista.annaSanat(), x);
+		while (hirsipuu.arvauksiaOnJaljella() > 0) {
+			System.out.println("anna kirjain: ");
+			Character c = sc.next().toUpperCase().charAt(0);
+			hirsipuu.arvaus(c);
+			System.out.println("");
+			System.out.println("arvaukset:" + hirsipuu.arvaukset());
+			System.out.println("tilanne: " + hirsipuu.tilanne());
+			System.out.println("arvauksia jäljellä: " + hirsipuu.arvauksiaOnJaljella());
+			System.out.println("");
+			if (hirsipuu.onLoppu() == true) {
+				System.out.println("Voitit pelin!");
+				break;
+			}
+		}
+		if (hirsipuu.arvauksiaOnJaljella() == 0) {
+			System.out.println("Hävisit pelin");
+			System.out.println("Oikea sana: " + hirsipuu.sana());
+		}
+	}
+}
